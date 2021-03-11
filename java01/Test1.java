72ef828ad3d189177cb7af8eaaf4b7b457550189
sano <t.sano@a-force.co.jp>
2021-03-11 11:03:05 +0900

    課題提出

--- java01/Test1.java
+++ java01/Test1.java
@@ -1,82 +1,65 @@
-
-
 import java.util.Scanner;
 
 public class Test1 {
-
 	public static void main(String[] args) {
 		System.out.println("一つ目の値を入力してください");
 		Scanner sc1 = new Scanner(System.in);
 		int num1 = sc1.nextInt();
 
 		System.out.println("二つ目の値を入力してください");
-		Scanner sc2=new Scanner(System.in);
-		int num2=sc2.nextInt();
+		Scanner sc2 = new Scanner(System.in);
+		int num2 = sc2.nextInt();
 
-		int total = num1+num2;
-		System.out.println("合計:"+total);
+		int total = num1 + num2;
+		System.out.println("合計:" + total);
 
-		int average = (num1+num2) / 2;
-		System.out.println("平均:" +average);
-
-
-
-		int height;
-		double weight;
+		int average = total / 2;
+		System.out.println("平均:" + average);
 
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("身長(cm):");
-		height = scanner.nextInt();
+		int height = scanner.nextInt();
 
 		System.out.println("体重(kg):");
-		weight = scanner.nextDouble();
+		double weight = scanner.nextDouble();
 
 		System.out.println();
 
 		double bmi = weight / (height * height) * 10000;
 
-		System.out.println("BMI : " + (int)bmi);
-
-		if(bmi<18.5) {
+		System.out.println("BMI : " +(int) bmi);
+		if(bmi < 18.5) {
 			System.out.println("あなたは「やせ」です");
-		}
-		else if(18.5 <= bmi && bmi <25.0) {
+		} else if(18.5 <= bmi && bmi < 25.0) {
 			System.out.println("あなたは「標準」です");
-		}
-		else if(25.0 <=bmi && bmi <30.0) {
+		} else if(25.0 <= bmi && bmi < 30.0) {
 			System.out.println("あなたは「肥満」です");
-		}
-		else if(30.0 <= bmi) {
+		} else if(30.0 <= bmi) {
 			System.out.println("あなたは「高度肥満」です");
 		}
 		scanner.close();
 
 
-		final int TOTAL_BORROWING = 250_000;
 		final double ANNUAL_INTEREST_RATE = 0.14;
-		final int MONTHLY_REPAYMENT_AMOUNT = 30_000;
+		final int MONTHLY_REPAYMENT_AMOUNT = 30000;
 
-		double remainingDebt = TOTAL_BORROWING;
-		for (int month = 1;remainingDebt > 0; month++) {
-			remainingDebt += remainingDebt*
-					ANNUAL_INTEREST_RATE/12;//年利分借金額を増やす
+		double remainingDebt = 250000;
+		for (int month = 1; remainingDebt > 0; month ++) {
+			remainingDebt += remainingDebt * ANNUAL_INTEREST_RATE / 12;//年利分借金額を増やす
 
-		double repaymentAmount = remainingDebt - MONTHLY_REPAYMENT_AMOUNT <= 0?
-			remainingDebt:MONTHLY_REPAYMENT_AMOUNT;remainingDebt -= repaymentAmount;//借金を支払う
-
-		boolean isRepaymentComplete = remainingDebt <= 0;
-			if(isRepaymentComplete) {
-				System.out.printf("%dヶ月目:返済額 = %f円。返済完了。",month,repaymentAmount);
-			}else {
-				System.out.printf("%dヶ月目:返済額 = %d円。残り%f円%n",month,(int)repaymentAmount,remainingDebt);
+			if (remainingDebt >= MONTHLY_REPAYMENT_AMOUNT) {
+				remainingDebt -= MONTHLY_REPAYMENT_AMOUNT;
+				System.out.printf(" %dヶ月目:返済額 = %d円。残り %f円 %n", month, MONTHLY_REPAYMENT_AMOUNT, remainingDebt);
+			} else {
+				System.out.printf(" %dヶ月目:返済額 = %f円。返済完了。", month, remainingDebt);
+				remainingDebt = 0;
 			}
-
-
 		}
-
-
-
-
 	}
 }
+
+
+
+
+
--- javascript/js/test.js
+++ javascript/js/test.js
@@ -1,44 +1,44 @@
-let element=document.getElementById("star");
-let star_html='<span>★</span>'
-for(let i=0;i<2;i++){
-	element.insertAdjacentHTML("afterbegin",star_html);
-	element.insertAdjacentHTML("beforeend",star_html);
-}
+test1 = document.getElementById('test1');
+var txt = "";
+	for (i = 0; i < 5; i++){
+		txt += "★";
+	}
+	test1.innerText = txt;
 
-o002=document.getElementById('002');
-for(let i=0;i<5;i++){
-	let s='';
-for(let j=0;j<5;j++){
-	s+=(i%2+j)%2==0?'☆':'★';
-}
-console.log(s);
-o002.innerText+=s+'\n';
+
+test2 = document.getElementById('test2');
+	for (let i = 0;i < 5;i ++ ) {
+		let s = '';
+			for (let j=0;j<5;j++) {
+				s += (i%2+j) % 2 == 0?'☆':'★';
+			}
+			test2.innerText += s + '\n';
 }
 
 	document.querySelector('#purchase').addEventListener('click',function(){
-	var result=document.querySelector('#result');
-	result.textContent="";
-	var price=0;
+	let result = document.querySelector('#result');
+	result.textContent = "";
+	let price = 0;
 
-	amount=document.querySelector('#amount').value;
-	product=document.querySelector('#product').value;
+	amount = document.querySelector('#amount').value;
+	product = document.querySelector('#product').value;
 
-	if(product=='コーヒー'){
+	if (product == 'コーヒー') {
 		price=150;
 	}
-	if(product=='お茶'){
+	if (product == 'お茶') {
 		price=130;
 	}
-	if(product=='オレンジジュース'){
+	if (product == 'オレンジジュース') {
 		price=160;
 	}
-	if(product=='水'){
+	if (product == '水') {
 		price=100;
 	}
 
-	if(amount>=price){
-		result.innerHTML=product+'を購入しました<br>おつりは'+(amount-price)+'円です'
+	if (amount >= price) {
+		result.innerHTML=product+'を購入しました<br>おつりは' + (amount - price) + '円です'
 	}else{
-		result.textContent='お金が足りません';
+		result.textContent = 'お金が足りません';
 	}
 });
